b7bcde0db78b1de9e1b5818235638c692699c72d
Prabhjot Singh <prabhjot.sookshminfo@gmail.com>
2020-03-31 20:53:03 +0530

    Revert "declare binderized soundtrigger."
    The still broken SoundTrigger implementation causes a bootloop in Q.

--- hidl.mk
+++ hidl.mk
@@ -16,7 +16,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
 		android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
     android.hardware.broadcastradio@1.0-impl
 
 # Camera
